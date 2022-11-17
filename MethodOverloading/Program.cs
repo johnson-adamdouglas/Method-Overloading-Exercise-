0aacd6dc9419efaf9a89ea7d8be40482f08099a2
Adam Johnson <johnson.adamdouglas@gmail.com>
2022-11-17 12:00:40 -0500

    updated

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -7,6 +7,16 @@
         {
             return num1 + num2;
         }
+        //params method
+        public static int Add(params int[] numbers)
+        {
+            var sum = 0;
+            foreach(int num in numbers)
+            {
+                sum += num;
+            }
+            return sum;
+        }
 
         public static decimal Add(decimal num1, decimal num2)
         {
@@ -15,18 +25,19 @@
 
         public static string Add(int num1, int num2, bool isTrue)
         {
+            var sum = num1 + num2;
             if (isTrue == true)
             {
-                if (num1 + num2 > 1)
+                if (sum > 1)
                 {
-                    return num1 + num2 + " dollars";
+                    return sum + " dollars";
                 }
                 else
                 {
-                    return num1 + num2 + " dollar";
+                    return sum + " dollar";
                 }
             }
-            else return "false";
+            else return sum.ToString();
         }
         static void Main(string[] args)
         {
